83df5656539b6ee448060f57743f85243af57af2
jekafile <jekafile@ya.ru>
2018-10-04 04:53:50 +0300

    first commit

---
+++ scripts/layout.js
@@ -0,0 +1,50 @@
+﻿
+$(document).ready(function(){
+	jQuery('#mycarousel-works').jcarousel({
+        scroll: 3
+    });	
+	
+	$('.fancybox').fancybox({
+		openEffect  : 'none',
+		closeEffect : 'none',
+
+		prevEffect : 'none',
+		nextEffect : 'none',
+
+		closeBtn  : false,
+
+		helpers : {
+			title : {
+				type : 'inside'
+			},
+			buttons	: {}
+		},
+
+		afterLoad : function() {
+			this.title = 'Изображение ' + (this.index + 1) + ' из ' + this.group.length + (this.title ? ' - ' + this.title : '');
+		}
+	});
+
+    $('.operator-mini-photo a').hover(function() {
+			$(this).find('ins').stop(true, true).fadeIn();
+    	}, function() {
+			$(this).find('ins').stop(true, true).fadeOut();
+    });		
+	
+
+	$('.search-form p a').click(function(){	
+		var value_a = $(this).attr('title');
+		$(this).parent().parent().find('input.pole').val(value_a);
+		
+		return false;
+	});		
+
+	$('.button-order').click(function() {
+		$('#basic-modal-order').modal({overlayClose:true});
+		return false;
+	});	
+	$('.modal-form-close').click(function(){
+		$.modal.close(); 
+	});		
+	
+});// конец ready function
